7ae38d6a5a4d6128b45d1a62e08e23810acb7c23
NebeSirgat <nebesirgat61@gmail.com>
2026-05-04 23:15:56 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+$correctEmail = "b251210577@sakarya.edu.tr";
+$cor rectPassword = "b251210577";
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+if (empty($username) || empty($password)) {
+    echo "<h2>Hata!</h2>";
+    echo "<p>Alanlar boş bırakılamaz.</p>";
+    echo "<a href='login.html'>Geri Dön</a>";
+} 
+else if ($username == $correctEmail && $password == $correctPassword) {
+    echo "<h2>Hoşgeldiniz B251210577</h2>";
+    echo "<p>Giriş başarılı.</p>";
+} 
+else {
+    echo "<h2>Hatalı Giriş!</h2>";
+    echo "<p>Kullanıcı adı veya şifre yanlış.</p>";
+    echo "<a href='login.html'>Tekrar Dene</a>";
+}
+?>
