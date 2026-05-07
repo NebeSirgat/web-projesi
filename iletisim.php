ac8a64fabe3c25d3ce0435d7ca79895fc9cf130c
NebeSirgat <nebesirgat61@gmail.com>
2026-05-07 13:04:31 +0300

    Add files via upload

--- iletisim.php
+++ iletisim.php
@@ -6,11 +6,12 @@ $cinsiyet = $_POST["cinsiyet"];
 $mesaj = $_POST["mesaj"];
 
 echo "<h2>İletişim Formu Bilgileri</h2>";
-echo "<p><strong>Ad Soyad:</strong> $adsoyad</p>";
+echo "<p><strong>Ad Soyad:</strong> $name</p>";
 echo "<p><strong>Email:</strong> $email</p>";
-echo "<p><strong>Telefon:</strong> $telefon</p>";
-echo "<p><strong>Cinsiyet:</strong> $cinsiyet</p>";
-echo "<p><strong>Mesaj:</strong> $mesaj</p>";
+echo "<p><strong>Telefon:</strong> $phone</p>";
+echo "<p><strong>Cinsiyet:</strong> $gender</p>";
+echo"<P><strong>hobiler:</strong> $hobby[]</p>";
+echo "<p><strong>Mesaj:</strong> $message</p>";
 
 if (isset($_POST["hobiler"])) {
     echo "<p><strong>Hobiler:</strong> ";
