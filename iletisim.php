ee131ee2905ff73625cfc2afbbab6b2accf7c3e7
NebeSirgat <nebesirgat61@gmail.com>
2026-05-09 23:51:34 +0300

    Add files via upload

--- iletisim.php
+++ iletisim.php
@@ -1,15 +1,17 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$gender = $_POST["gender"];
+$name    = $_POST["name"];
+$email   = $_POST["email"];
+$phone   = $_POST["phone"];
+$gender  = $_POST["gender"];
 $message = $_POST["message"];
+$sehir   = $_POST["sehir"];
 
 echo "<h2>İletişim Formu Bilgileri</h2>";
 echo "<p><strong>Ad Soyad:</strong> $name</p>";
 echo "<p><strong>Email:</strong> $email</p>";
 echo "<p><strong>Telefon:</strong> $phone</p>";
 echo "<p><strong>Cinsiyet:</strong> $gender</p>";
+echo "<p><strong>Şehir:</strong> $sehir</p>";
 echo "<p><strong>Mesaj:</strong> $message</p>";
 
 if (isset($_POST["hobby"])) {
